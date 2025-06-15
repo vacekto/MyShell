0088f99a1af49ed63ded28a7f28c1db1776db67b
Magic Turtle <vacekto@gmail.com>
2025-06-16 00:16:51 +0200

    fixed memory leaks

--- util.c
+++ util.c
@@ -15,3 +15,24 @@ Redirection *create_redirection(RedirType type, int source_fd, int target_fd, ch
     redir->flags = flags;
     return redir;
 }
+
+void free_commands(Command **commands)
+{
+    for (int i = 0; commands[i] != NULL; i++)
+    {
+
+        for (int l = 0; l < commands[i]->redir_count; l++)
+        {
+            free(commands[i]->redirs[l]->filename);
+            free(commands[i]->redirs[l]);
+        }
+        free(commands[i]->redirs);
+
+        for (int j = 0; commands[i]->args[j] != NULL; j++)
+        {
+            free(commands[i]->args[j]);
+        }
+        free(commands[i]->args);
+        free(commands[i]);
+    }
+}
